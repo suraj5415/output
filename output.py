f68e96581a00da12eda454f0d1a735fd0ae2edb3
suraj5415 <113723288+suraj5415@users.noreply.github.com>
2022-09-17 23:49:27 +0530

    Create output.py

---
+++ output.py
@@ -0,0 +1,18 @@
+['bread', 1, 'eggs', 12, 'peanut butter', 1, 'chicken', 2]
+['bread', 1, 'paneer', 'eggs', 12, 'peanut butter', 1, 'chicken', 2]
+['bread', 1, 'paneer', 'eggs', 12, 'peanut butter', 1, 'chicken', 2, 'maggie masala']
+['bread', 'paneer', 'eggs', 12, 'peanut butter', 1, 'chicken', 2, 'maggie masala']
+['bread', 'paneer', 12, 'peanut butter', 1, 'chicken', 2, 'maggie masala']
+('maths 12/15', 'english 10/15', 'hindi 13/15')
+{'instagram id': '_sooraj_sj_007', 'password': 'suraj1234', 'followers': '1k', 'posts': '60'}
+{'instagram id': '_sooraj_sj_007', 'password': 'suraj1234', 'followers': '1k'}
+{'instagram id': '_sooraj_sj_007', 'password': 'suraj1234', 'followers': '1k', 'likes': '10800'}
+{'instagram id': '_sooraj_sj_007', 'password': 'suraj1234', 'followers': '1.1k', 'likes': '10800'}
+{'instagram id': '_sooraj_sj_007', 'password': 'suraj1234', 'followers': '1.1k', 'likes': '10800', 'posts': '35'}
+blackpipers
+black
+piper
+blackpipers
+bakies
+srepipkcalb
+calb
